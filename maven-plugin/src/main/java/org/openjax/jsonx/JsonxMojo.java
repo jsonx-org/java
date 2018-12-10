bd208a9f4ba0a4c12d4caa77ecd952a9498de95f
Seva Safris <seva@safris.org>
2018-12-10 17:49:08 +0700

    Implement JsonxMojo

--- maven-plugin/src/main/java/org/openjax/jsonx/JsonxMojo.java
+++ maven-plugin/src/main/java/org/openjax/jsonx/JsonxMojo.java
@@ -16,6 +16,9 @@
 
 package org.openjax.jsonx;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
 
 import org.apache.maven.plugin.MojoExecutionException;
@@ -26,22 +29,30 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.fastjax.maven.mojo.GeneratorMojo;
 import org.fastjax.maven.mojo.SourceInput;
+import org.fastjax.xml.ValidationException;
+import org.openjax.jsonx.generator.Schema;
+import org.openjax.jsonx.jsonx_0_9_8.xL3gluGCXYYJc.Jsonx;
+import org.openjax.xsb.runtime.Bindings;
 
-@Mojo(name="generate", defaultPhase=LifecyclePhase.GENERATE_SOURCES)
-@Execute(goal="generate")
+@Mojo(name = "generate", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
+@Execute(goal = "generate")
 public class JsonxMojo extends GeneratorMojo {
   @SourceInput
-  @Parameter(property="schemas", required=true)
+  @Parameter(property = "schemas", required = true)
   private List<String> schemas;
 
   @Override
   public void execute(final Configuration configuration) throws MojoExecutionException, MojoFailureException {
-//    try {
-//      for (final URL resource : configuration.getResources(0))
-//        Generator.generate(resource, configuration.getDestDir(), false);
-//    }
-//    catch (final GeneratorExecutionException | IOException e) {
-//      throw new MojoExecutionException(e.getMessage(), e);
-//    }
+    try {
+      for (final URL resource : configuration.getSourceInputs("schemas")) {
+        try (final InputStream in = resource.openStream()) {
+          final Schema schema = new Schema((Jsonx)Bindings.parse(in));
+          schema.toSource(configuration.getDestDir());
+        }
+      }
+    }
+    catch (final IOException | ValidationException e) {
+      throw new MojoExecutionException(e.getMessage(), e);
+    }
   }
 }
